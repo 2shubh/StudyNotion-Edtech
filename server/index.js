037ba03b0a7364a36e0c3e7c8ece7548005541c2
2shubh <shubhamgond7678@gmail.com>
2025-04-05 03:08:31 +0530

    Updated cors

--- server/index.js
+++ server/index.js
@@ -23,10 +23,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:["http://localhost:3000","https://studynotion-edtech-five.vercel.app/"],
-		credentials:true,
+	  origin: ["http://localhost:3000", "https://studynotion-edtech-five.vercel.app"],
+	  credentials: true,
 	})
-)
+  );
 
 app.use(
 	fileUpload({
