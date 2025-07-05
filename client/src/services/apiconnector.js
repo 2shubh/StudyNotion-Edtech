fdf860b38ac941bc06ea36842673b2ae1cd3aca4
Shubham <shubhamgond7678@gmail.com>
2025-07-05 13:30:41 +0530

    Refactor folder structure, update base URL, and improve .gitignore

--- client/src/services/apiconnector.js
+++ client/src/services/apiconnector.js
@@ -2,7 +2,9 @@ import axios from "axios"
 
 export const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
+    
 });
+
   
 
 console.log("BASE_URL:", process.env.REACT_APP_BASE_URL);
